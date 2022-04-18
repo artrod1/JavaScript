b85a400ba53bd788888cec653809cf4537080cb3
artrod1 <103657452+artrod1@users.noreply.github.com>
2022-04-18 22:36:52 +0300

    Объекты, циклы, if / else

--- homework.js
+++ homework.js
@@ -1,6 +1,29 @@
 
+const _ = function(e){
+    console.log(e);
+}
+
 let randomWord = ["один", "два", "три"];
 let magic = randomWord[Math.floor(Math.random() * 2)];
 console.log(magic + " привет луна " + magic + " слон " + magic);
 
 console.log(randomWord.join(" + "));
+
+randomWord.join("больше")
+_(randomWord.join("больше"));
+
+//_(magic.join());
+
+let bor = {
+    "key1": 5,
+    "key2": 10
+}
+
+_(bor.key1); // точечная натация
+_(bor["key1"]) //обращение по ключу объекта
+
+if(2 > 5) {
+    _("test= " + true);
+} else{
+    _(false);
+}
