737f1cdb586e705a0e531176e3df2ae00b289dc5
artrod1 <103657452+artrod1@users.noreply.github.com>
2022-04-18 22:04:41 +0300

    dz

--- homework.js
+++ homework.js
@@ -2,3 +2,5 @@
 let randomWord = ["один", "два", "три"];
 let magic = randomWord[Math.floor(Math.random() * 2)];
 console.log(magic + " привет луна " + magic + " слон " + magic);
+
+console.log(randomWord.join(" + "));
